1d536e67732ecd3e4708c62e3cefbc3378536870
NaoRiley <rileydean.v3@gmail.com>
2025-04-25 20:16:23 -0700

    hotfix

--- items/joker.lua
+++ items/joker.lua
@@ -171,53 +171,53 @@ SMODS.Joker{
 	end
 }
 
-SMODS.Joker{
-    key = 'carrytest',
-    loc_txt = {
-        name = 'Carry Test',
-        text = {
-            'Bring extra {C:attention}Score {C:black}you win to the Next Blind',
-			'{C:inactive}(Currently {C:purple}+#1# {C:inactive}Score)',
-        }
-    },
-    atlas = 'j_placeholder',
-    pos = {x = 0, y = 0},
-	blueprint_compat = false,
-	config = {
-		extra = {
-			score = 0,
-		}
-	},
-	loc_vars = function(self, info_queue, card)
-		return {
-			vars = {
-				card.ability.extra.score}
-		}
-	end,
-	calculate = function(self, card, context)
-		if context.after then
-			G.E_MANAGER:add_event(Event({
-				func = function() 
-					G.GAME.chips = (to_big(G.GAME.chips))+(to_big(card.ability.extra.score)),
-					G.HUD:get_UIE_by_ID('chip_UI_count'):juice_up(0.3, 0.3)
-					play_sound('multhit1')
-					play_sound('chips1')
-					card.ability.extra.score = (to_big(0))
-					return true
-				end,
-			}))
-		end
-		if context.blind_defeated and not context.repetition or context.blueprint then
-			card.ability.extra.score = (to_big(G.GAME.chips))-(to_big(G.GAME.blind.chips))
-		end
-		if context.after then
-			return {
-				message = "+" .. tostring(card.ability.extra.score),
-				colour = G.C.PURPLE
-			}
-		end
-	end
-}
+-- SMODS.Joker{
+--     key = 'carrytest',
+--     loc_txt = {
+--         name = 'Carry Test',
+--         text = {
+--             'Bring extra {C:attention}Score {C:black}you win to the Next Blind',
+-- 			'{C:inactive}(Currently {C:purple}+#1# {C:inactive}Score)',
+--         }
+--     },
+--     atlas = 'j_placeholder',
+--     pos = {x = 0, y = 0},
+-- 	blueprint_compat = false,
+-- 	config = {
+-- 		extra = {
+-- 			score = 0,
+-- 		}
+-- 	},
+-- 	loc_vars = function(self, info_queue, card)
+-- 		return {
+-- 			vars = {
+-- 				card.ability.extra.score}
+-- 		}
+-- 	end,
+-- 	calculate = function(self, card, context)
+-- 		if context.after then
+-- 			G.E_MANAGER:add_event(Event({
+-- 				func = function() 
+-- 					G.GAME.chips = (to_big(G.GAME.chips))+(to_big(card.ability.extra.score)),
+-- 					G.HUD:get_UIE_by_ID('chip_UI_count'):juice_up(0.3, 0.3)
+-- 					play_sound('multhit1')
+-- 					play_sound('chips1')
+-- 					card.ability.extra.score = (to_big(0))
+-- 					return true
+-- 				end,
+-- 			}))
+-- 		end
+-- 		if context.blind_defeated and not context.repetition or context.blueprint then
+-- 			card.ability.extra.score = (to_big(G.GAME.chips))-(to_big(G.GAME.blind.chips))
+-- 		end
+-- 		if context.after then
+-- 			return {
+-- 				message = "+" .. tostring(card.ability.extra.score),
+-- 				colour = G.C.PURPLE
+-- 			}
+-- 		end
+-- 	end
+-- }
 
 SMODS.Joker{
     key = 'warlock',
@@ -511,7 +511,7 @@ SMODS.Joker{
 				end,
 			}))
 		end
-		if context.after and not context.discard then
+		if context.after then
 			return {
 				message = "X" .. tostring(card.ability.extra.score),
 				colour = G.C.PURPLE
