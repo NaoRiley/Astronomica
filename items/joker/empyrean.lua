3b32fc0b9d2064f806dd48836bf0ae7a43334f55
NaoRiley <rileydean.v3@gmail.com>
2025-05-30 22:34:35 -0700

    Add Alfheim, make Radalingr not suck

--- items/joker/empyrean.lua
+++ items/joker/empyrean.lua
@@ -60,13 +60,112 @@
 -- end
 -- }
 
+-- SMODS.Joker {
+-- 	key = "radalingr", 
+-- 	loc_txt = {
+-- 		name = "{f:ast_futhark2,s:1.1,C:chips}Radalingr",
+-- 		text = {
+-- 			"At end of round, Joker's {X:mult,C:white}XMult{C:black} becomes {X:dark_edition,C:white}(XMult)! ",
+-- 			"{C:inactive}(Currently {X:mult,C:white}X#1#{}{C:inactive} Mult)",
+-- 		}
+-- 	},
+-- 	pos = { x = 0, y = 0 },
+-- 	soul_pos = { x = 2, y = 0, extra = { x = 1, y = 0 } },
+-- 	cost = 100,
+-- 	rarity = 'ast_empyrean',
+-- 	unlocked = true,
+-- 	discovered = true,
+-- 	blueprint_compat = true,
+-- 	demicoloncompat = true,
+-- 	eternal_compat = true,
+-- 	perishable_compat = true,
+-- 	immutable = false,
+-- 	atlas = "exotic",
+-- 	ast_credits = {
+-- 		art = {"Tatteredlurker"},
+-- 	},
+-- 	config = {
+-- 		extra = {
+-- 			n = 3, 
+-- 			c = false
+-- 		},
+-- 	},
+-- 	loc_vars = function(self, info_queue, card)
+-- 		return {
+-- 			vars = {card.ability.extra.n}
+-- 		}
+-- 	end,
+-- 	calculate = function(self, card, context) --i now see why nobody has done factorials in this game before
+-- 		--print('calculate called')
+-- 		if context.first_hand_drawn then
+-- 			card.ability.extra.c = false
+-- 		end
+-- 		if context.end_of_round and card.ability.extra.c == false then
+-- 			--print('round end')
+-- 			card.ability.extra.c = true
+-- 			if to_big(card.ability.extra.n):gt(to_big(150)) then
+-- 				card.ability.extra.n = UTIL_TABLE.factorial(card.ability.extra.n)
+-- 			else
+-- 				card.ability.extra.n = UTIL_TABLE.small_factorial(card.ability.extra.n)
+-- 			end
+-- 		end
+-- 		if context.forcetrigger then
+-- 			--print('round end')
+-- 			card.ability.extra.c = true
+-- 			if to_big(card.ability.extra.n):gt(to_big(150)) then
+-- 				card.ability.extra.n = UTIL_TABLE.factorial(card.ability.extra.n)
+-- 			else
+-- 				card.ability.extra.n = UTIL_TABLE.small_factorial(card.ability.extra.n)
+-- 			end
+-- 			card.ability.extra.c = false
+-- 		end
+-- 		if context.joker_main then
+-- 			return {Xmult_mod = card.ability.extra.n, message = "X" .. tostring(card.ability.extra.n)}
+-- 		end
+-- 		if context.end_of_round and context.game_over == false and not context.repetition and not context.blueprint then
+-- 			return {
+-- 				message = 'Upgrade!',
+-- 			colour = G.C.DARK_EDITION,
+-- 			card = card
+-- 			}
+-- 		end
+-- 	end,
+-- 	generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
+--     SMODS.Center.generate_ui(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
+
+--     if desc_nodes == full_UI_table.main then
+--         full_UI_table.name = {
+--             {
+--                 n = G.UIT.C,
+--                 config = { align = "cm", padding = 0.05 },
+--                 nodes = {
+--                     {
+--                         n = G.UIT.R,
+--                         config = { align = "cm" },
+--                         nodes = full_UI_table.name
+--                     },
+--                     {
+--                         n = G.UIT.R,
+--                         config = { align = "cm" },
+--                         nodes = {{
+--                           n = G.UIT.T,
+--                           config = { text = "(Radąlingr)", colour = G.C.INACTIVE, scale = (0.4 - 0.004 * #"(Radąlingr)") } }
+--                     }},
+--                 }
+--             }
+--         }
+--     end
+-- end
+-- }
+
 SMODS.Joker {
 	key = "radalingr", 
 	loc_txt = {
 		name = "{f:ast_futhark2,s:1.1,C:chips}Radalingr",
 		text = {
-			"At end of round, Joker's {X:mult,C:white}XMult{C:black} becomes {X:dark_edition,C:white}(XMult)! ",
+			"When triggered, Joker's {X:mult,C:white}XMult{C:black} becomes {X:dark_edition,C:white}XMult!",
 			"{C:inactive}(Currently {X:mult,C:white}X#1#{}{C:inactive} Mult)",
+			"{C:inactive,s:0.8}Example: {X:dark_edition,C:white,s:0.8}3!{} ={C:inactive,s:0.8} {C:attention,s:0.8}3x2x1{C:inactive,s:0.8} (6)"
 		}
 	},
 	pos = { x = 0, y = 0 },
@@ -100,16 +199,7 @@ SMODS.Joker {
 		if context.first_hand_drawn then
 			card.ability.extra.c = false
 		end
-		if context.end_of_round and card.ability.extra.c == false then
-			--print('round end')
-			card.ability.extra.c = true
-			if to_big(card.ability.extra.n):gt(to_big(150)) then
-				card.ability.extra.n = UTIL_TABLE.factorial(card.ability.extra.n)
-			else
-				card.ability.extra.n = UTIL_TABLE.small_factorial(card.ability.extra.n)
-			end
-		end
-		if context.forcetrigger then
+		if context.pre_joker and card.ability.extra.c == false or context.forcetrigger then
 			--print('round end')
 			card.ability.extra.c = true
 			if to_big(card.ability.extra.n):gt(to_big(150)) then
@@ -118,16 +208,14 @@ SMODS.Joker {
 				card.ability.extra.n = UTIL_TABLE.small_factorial(card.ability.extra.n)
 			end
 			card.ability.extra.c = false
-		end
-		if context.joker_main then
-			return {Xmult_mod = card.ability.extra.n, message = "X" .. tostring(card.ability.extra.n)}
-		end
-		if context.end_of_round and context.game_over == false and not context.repetition and not context.blueprint then
 			return {
 				message = 'Upgrade!',
 			colour = G.C.DARK_EDITION,
 			card = card
 			}
+		end
+		if context.joker_main then
+			return {Xmult_mod = card.ability.extra.n, message = "X" .. tostring(card.ability.extra.n)}
 		end
 	end,
 	generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
@@ -163,8 +251,8 @@ SMODS.Joker {
 	loc_txt = {
 		name = "{f:ast_futhark2,s:1.1,C:chips}Aukatigi",
 		text = {
-			"After scoring, the total chips and mult",
-			"replaces the played poker hand's base chips and mult",
+			"After scoring, the total {X:chips,C:white}Chips{} and {X:mult,C:white}Mult{}",
+			"replaces the played poker hand's base {X:chips,C:white}Chips{} and {X:mult,C:white}Mult{}",
 		}
 	},
 	pos = { x = 6, y = 1 },
@@ -233,6 +321,89 @@ end
 }
 
 SMODS.Joker {
+	key = "alfheim", 
+	loc_txt = {
+		name = "{f:ast_futhark2,s:1.1,C:chips}Alfheim",
+		text = {
+			'Gain {X:dark_edition,C:white}^Chips{} for each scored {C:spades}Spade{} and {C:clubs}Club{}',
+			'Gain {X:dark_edition,C:white}^Mult{} for each scored {C:hearts}Heart{} and {C:diamonds}Diamond{}',
+			'{X:chips,C:white}Chips{} and {X:mult,C:white}Mult{} scale exponentially',
+			"Joker's scoring effects trigger per card played",
+			"{C:inactive}(Currently {X:dark_edition,C:white}^#1# {C:inactive} Chips and {X:dark_edition,C:white}^#2# {C:inactive} Mult)",
+		}
+	},
+	pos = { x = 0, y = 2 },
+	soul_pos = { x = 2, y = 2, extra = { x = 1, y = 2 } },
+	cost = 100,
+	rarity = 'ast_empyrean',
+	unlocked = true,
+	discovered = true,
+	blueprint_compat = true,
+	demicoloncompat = true,
+	eternal_compat = true,
+	perishable_compat = true,
+	immutable = false,
+	atlas = "exotic",
+	ast_credits = {
+		art = {"Tatteredlurker"},
+	},
+	config = {
+		extra = {
+			echips = 2,
+			emult = 2,
+		},
+	},
+	loc_vars = function(self, info_queue, card)
+		return {
+			vars = {
+				card.ability.extra.echips,
+				card.ability.extra.emult,
+			}
+		}
+	end,
+	calculate = function(self, card, context)
+		if (context.cardarea == G.play and context.individual and context.other_card:is_suit("Spades")) or (context.cardarea == G.play and context.individual and context.other_card:is_suit("Clubs")) or context.forcetrigger then
+            card.ability.extra.echips = (to_big(card.ability.extra.echips)) ^ (to_big(2))
+        end
+		if (context.cardarea == G.play and context.individual and context.other_card:is_suit("Hearts")) or (context.cardarea == G.play and context.individual and context.other_card:is_suit("Diamonds")) or context.forcetrigger then
+            card.ability.extra.emult = (to_big(card.ability.extra.emult)) ^ (to_big(2))
+        end
+		if (context.individual and context.cardarea == G.play) or context.forcetrigger then
+            return {
+				emult = card.ability.extra.emult, 
+				echips = card.ability.extra.echips,
+			}
+		end
+	end,
+	generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
+    SMODS.Center.generate_ui(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
+
+    if desc_nodes == full_UI_table.main then
+        full_UI_table.name = {
+            {
+                n = G.UIT.C,
+                config = { align = "cm", padding = 0.05 },
+                nodes = {
+                    {
+                        n = G.UIT.R,
+                        config = { align = "cm" },
+                        nodes = full_UI_table.name
+                    },
+                    {
+                        n = G.UIT.R,
+                        config = { align = "cm" },
+                        nodes = {{
+                          n = G.UIT.T,
+                          config = { text = "(Alfheim)", colour = G.C.INACTIVE, scale = (0.4 - 0.004 * #"(Alfheim)") } }
+                    }},
+                }
+            }
+        }
+    end
+end
+}
+
+SMODS.Joker {
 	key = "asgard", 
 	loc_txt = {
 		name = "{f:ast_futhark2,s:1.1,C:chips}Asgard",
@@ -253,7 +424,6 @@ SMODS.Joker {
 	demicoloncompat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-	immutable = false, --worth a try
 	atlas = "exotic",
 	ast_credits = {
 		art = {"Tatteredlurker"},
@@ -261,11 +431,11 @@ SMODS.Joker {
 	config = {
         extra = {
 			chips = 1,
-			chips_mod = 1,
-			cap = 100,
 			fakechips = 1e300,
 			immutable = {
 				arrows = -1,
+				chips_mod = 1,
+				cap = 100,
 			},
         },
     },
@@ -273,19 +443,19 @@ SMODS.Joker {
 		return {
 			vars = {
                 ast.FormatArrowMult(card.ability.extra.immutable.arrows, card.ability.extra.chips),
-                card.ability.extra.chips_mod,
+                card.ability.extra.immutable.chips_mod,
 				card.ability.extra.immutable.arrows, --??????????
-                card.ability.extra.cap,
+                card.ability.extra.immutable.cap,
             },
 		}
 	end,
 	calculate = function(self, card, context)
-		if ((to_big(card.ability.extra.chips)) >= (to_big(card.ability.extra.cap)) and not context.repetition and not context.blueprint) or context.forcetrigger then
+		if ((to_big(card.ability.extra.chips)) >= (to_big(card.ability.extra.immutable.cap)) and not context.repetition and not context.blueprint) or context.forcetrigger then
 			card.ability.extra.immutable.arrows = (to_big(card.ability.extra.immutable.arrows)) + (to_big(1))
 			card.ability.extra.chips = (to_big(1))
 		end
 		if not context.blueprint and ((context.post_trigger and context.other_joker ~= card) or (context.individual and context.cardarea == G.play)) then --duplicare type beat
-			card.ability.extra.chips = lenient_bignum(to_big(card.ability.extra.chips) + card.ability.extra.chips_mod)
+			card.ability.extra.chips = lenient_bignum(to_big(card.ability.extra.chips) + card.ability.extra.immutable.chips_mod)
 			card_eval_status_text(card, "extra", nil, nil, nil, { message = localize("k_upgrade_ex") })
 		end
 		if (context.individual and (to_big(card.ability.extra.immutable.arrows)) <= (to_big(100)) and context.cardarea == G.play) or (context.forcetrigger and (to_big(card.ability.extra.immutable.arrows)) <= (to_big(100))) then
--- items/joker/rare.lua
+++ items/joker/rare.lua
@@ -1,5 +1,5 @@
 SMODS.Joker {
-	key = 'facsimile', --code mostly copied from old blueprint in cryptid with the banana code removed i dont know how 95% of this works
+	key = 'facsimile', --code formerly copied from old blueprint, now IMMENSELY optimized thanks to VanillaRemade
 	loc_txt = {
 		name = 'Facsimile',
 		text = {
@@ -8,107 +8,44 @@ SMODS.Joker {
 		}
 	},
 	pos = { x = 0, y = 0 },
-	config = { extra = { odds = 0 } }, --note to self DONT delete this i have NO idea why but deleting this crashes the fucking game
 	rarity = 3,
 	cost = 10,
-	update = function(self, card, front)
-		if G.STAGE == G.STAGES.RUN then
-			for i = 1, #G.jokers.cards do
-				if G.jokers.cards[i] == card then
-					other_joker = G.jokers.cards[i - 1]
-				end
-			end
-			if other_joker and other_joker ~= card and other_joker.config.center.blueprint_compat then
-				card.ability.blueprint_compat = "compatible"
-			else
-				card.ability.blueprint_compat = "incompatible"
-			end
-		end
-	end,
-	loc_vars = function(self, info_queue, card)
-		card.ability.blueprint_compat_ui = card.ability.blueprint_compat_ui or ""
-		card.ability.blueprint_compat_check = nil
-		return {
-			vars = {
-				cry_prob(card.ability.cry_prob, card.ability.extra.odds, card.ability.cry_rigged),
-				card.ability.extra.odds,
-			},
-			main_end = (card.area and card.area == G.jokers) and {
-				{
-					n = G.UIT.C,
-					config = { align = "bm", minh = 0.4 },
-					nodes = {
-						{
-							n = G.UIT.C,
-							config = {
-								ref_table = card,
-								align = "m",
-								colour = G.C.JOKER_GREY,
-								r = 0.05,
-								padding = 0.06,
-								func = "blueprint_compat",
-							},
-							nodes = {
-								{
-									n = G.UIT.T,
-									config = {
-										ref_table = card.ability,
-										ref_value = "blueprint_compat_ui",
-										colour = G.C.UI.TEXT_LIGHT,
-										scale = 0.32 * 0.8,
-									},
-								},
-							},
-						},
-					},
-				},
-			} or nil,
-		}
-	end,
 	blueprint_compat = true,
 	eternal_compat = true,
 	atlas = 'jokers',
-	calculate = function(self, card, context)
-	if not context.blueprint or (context.blueprint and context.fromRight) then --prevents the crash
-			local other_joker = nil
-			for i = 1, #G.jokers.cards do
-				if G.jokers.cards[i] == card then
-					other_joker = G.jokers.cards[i - 1]
-				end
-			end
-			if other_joker and other_joker ~= card then
-				context.blueprint = (context.blueprint and (context.blueprint + 1)) or 1
-				context.blueprint_card = context.blueprint_card or card
-				context.fromRight = true
-
-				if context.blueprint > #G.jokers.cards + 1 then
-					return
-				end
-
-				local other_joker_ret, trig = other_joker:calculate_joker(context)
-				local eff_card = context.blueprint_card or card
-
-				context.blueprint = nil
-				context.blueprint_card = nil
-				context.fromRight = nil
-
-				if other_joker_ret == true then
-					return other_joker_ret
-				end
-				if other_joker_ret or trig then
-					if not other_joker_ret then
-						other_joker_ret = {}
-					end
-					other_joker_ret.card = eff_card
-					other_joker_ret.colour = darken(G.C.BLUE, 0.3)
-					other_joker_ret.no_callback = true
-					return other_joker_ret
-				end
-			end
-		end
-	end,
+	loc_vars = function(self, info_queue, card)
+        if card.area and card.area == G.jokers then
+            local other_joker
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i] == card then other_joker = G.jokers.cards[i - 1] end
+            end
+            local compatible = other_joker and other_joker ~= card and other_joker.config.center.blueprint_compat
+            main_end = {
+                {
+                    n = G.UIT.C,
+                    config = { align = "bm", minh = 0.4 },
+                    nodes = {
+                        {
+                            n = G.UIT.C,
+                            config = { ref_table = card, align = "m", colour = compatible and mix_colours(G.C.GREEN, G.C.JOKER_GREY, 0.8) or mix_colours(G.C.RED, G.C.JOKER_GREY, 0.8), r = 0.05, padding = 0.06 },
+                            nodes = {
+                                { n = G.UIT.T, config = { text = ' ' .. localize('k_' .. (compatible and 'compatible' or 'incompatible')) .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
+                            }
+                        }
+                    }
+                }
+            }
+            return { main_end = main_end }
+        end
+    end,
+    calculate = function(self, card, context)
+        local other_joker = nil
+        for i = 1, #G.jokers.cards do
+            if G.jokers.cards[i] == card then other_joker = G.jokers.cards[i - 1] end
+        end
+        return SMODS.blueprint_effect(card, other_joker, context)
+    end,
 	ast_credits = {
-		code = {"MathIsFun"},
 		art = {'MarioFan597'},
 	},
 }
