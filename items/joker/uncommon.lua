61032dec26c767d763151ed7b05d6525a8311f1c
NaoRiley <rileydean.v3@gmail.com>
2025-06-10 21:17:55 -0700

    whoops

---
+++ items/joker/uncommon.lua
@@ -0,0 +1,101 @@
+SMODS.Joker{
+    key = 'genie',
+    loc_txt = {
+        name = 'Genie Joker',
+        text = {
+            "Increase values of {C:attention}Joker{} to the right",
+			"by {C:attention}+#1#{} at end of round",
+        }
+    },
+	blueprint_compat = true,
+    atlas = 'j_placeholder',
+    pos = {x = 0, y = 0},
+	rarity = 2,
+	order = 1,
+	config = {
+		extra = {
+			increase = 4
+		}
+	},
+	loc_vars = function(self, info_queue, card)
+		card.ability.blueprint_compat_ui = card.ability.blueprint_compat_ui or ""
+		card.ability.blueprint_compat_check = nil
+		return {
+			vars = { number_format(card.ability.extra.increase) },
+			main_end = (card.area and card.area == G.jokers) and {
+				{
+					n = G.UIT.C,
+					config = { align = "bm", minh = 0.4 },
+					nodes = {
+						{
+							n = G.UIT.C,
+							config = {
+								ref_table = card,
+								align = "m",
+								colour = G.C.JOKER_GREY,
+								r = 0.05,
+								padding = 0.06,
+								func = "blueprint_compat",
+							},
+							nodes = {
+								{
+									n = G.UIT.T,
+									config = {
+										ref_table = card.ability,
+										ref_value = "blueprint_compat_ui",
+										colour = G.C.UI.TEXT_LIGHT,
+										scale = 0.32 * 0.8,
+									},
+								},
+							},
+						},
+					},
+				},
+			} or nil,
+		}
+	end,
+	update = function(self, card, front)
+		if G.STAGE == G.STAGES.RUN then
+			for i = 1, #G.jokers.cards do
+				if G.jokers.cards[i] == card then
+					other_joker = G.jokers.cards[i + 1]
+				end
+			end
+			if other_joker and other_joker ~= card and not (Card.no(other_joker, "immutable", true)) then
+				card.ability.blueprint_compat = "compatible"
+			else
+				card.ability.blueprint_compat = "incompatible"
+			end
+		end
+	end,
+	calculate = function(self, card, context)
+		if
+			(context.end_of_round and not context.repetition and not context.individual and not context.blueprint)
+			or context.forcetrigger
+		then
+			local check = false
+			for i = 1, #G.jokers.cards do
+				if G.jokers.cards[i] == card then
+					if i < #G.jokers.cards then
+						if not Card.no(G.jokers.cards[i + 1], "immutable", true) then
+							check = true
+							Cryptid.with_deck_effects(G.jokers.cards[i + 1], function(cards)
+								Cryptid.manipulate(cards, { value = card.ability.extra.increase, type = "+" })
+							end)
+						end
+					end
+				end
+			end
+			if check then
+				card_eval_status_text(
+					card,
+					"extra",
+					nil,
+					nil,
+					nil,
+					{ message = localize("k_upgrade_ex"), colour = G.C.GREEN }
+				)
+			end
+		end
+	end,
+}
